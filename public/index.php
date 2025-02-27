f855dfa6923e196a2f9eb446533d5c26f4a021cb
Julian Salazar <jsalazar@sena.edu.co>
2025-02-27 14:57:48 -0500

    Test production 2

--- public/index.php
+++ public/index.php
@@ -1,36 +1,36 @@
 <?php
-echo "HOla";
-// require_once '../app/config/global.php';
-// require_once '../app/controllers/homeController.php';
-// require_once '../app/controllers/rolController.php';
-// require_once '../app/controllers/centroController.php';
-// require_once '../app/controllers/actividadController.php';
-// require_once '../app/controllers/programaController.php';
+
+require_once '../app/config/global.php';
+require_once '../app/controllers/homeController.php';
+require_once '../app/controllers/rolController.php';
+require_once '../app/controllers/centroController.php';
+require_once '../app/controllers/actividadController.php';
+require_once '../app/controllers/programaController.php';
 
 
-// $url = $_SERVER['REQUEST_URI']; // Lo que se ingresa en la url
+$url = $_SERVER['REQUEST_URI']; // Lo que se ingresa en la url
 
-// $routes = include_once '../app/config/routes.php';
+$routes = include_once '../app/config/routes.php';
 
-// $matchedRoute = null;
-// foreach ($routes as $route => $routeConfig) {
-//     if (preg_match("#^$route$#", $url, $matches)) {
-//         $matchedRoute = $routeConfig;
-//         break;
-//     }
-// }
+$matchedRoute = null;
+foreach ($routes as $route => $routeConfig) {
+    if (preg_match("#^$route$#", $url, $matches)) {
+        $matchedRoute = $routeConfig;
+        break;
+    }
+}
 
-// if ($matchedRoute) {
-//     $controllerName = $matchedRoute['controller'];
-//     $actionName = $matchedRoute['action'];
-//     if (class_exists($controllerName) && method_exists($controllerName, $actionName)) {
-//         // Obtener parametros por URL
-//         $parameters = array_slice($matches, 1);
-//         $controller = new $controllerName();
-//         $controller->$actionName(...$parameters);
-//         exit;
-//     }
-// }
+if ($matchedRoute) {
+    $controllerName = $matchedRoute['controller'];
+    $actionName = $matchedRoute['action'];
+    if (class_exists($controllerName) && method_exists($controllerName, $actionName)) {
+        // Obtener parametros por URL
+        $parameters = array_slice($matches, 1);
+        $controller = new $controllerName();
+        $controller->$actionName(...$parameters);
+        exit;
+    }
+}
 
 // if (array_key_exists($url, $routes)) {
 //     // $controller = new HomeController();
